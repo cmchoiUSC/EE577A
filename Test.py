b1da983ecf1f2173a46f2bfc2131f82951e2b60f
Colin <cmchoi@usc.edu>
2024-09-06 15:07:42 -0700

    early stage code

--- Test.py
+++ Test.py
@@ -1,4 +1,51 @@
-print("hello world")
+# Test file for EE577A Lab 1
+
+import re
+
+input_file = 'LiberateResults.txt'
+# output_file = 'Results.txt'
 
 
-## final test
+def extract_cell_names(input_file_path):
+    try:
+        with open(input_file_path, 'r') as input_file:
+                content = input_file.read()
+
+                # Use a regular expression pattern to find "cell (**name**)" occurrences
+                pattern = r"cell \("
+                cell_names = re.findall(pattern, content)
+
+                # Print the extracted cell names and write them to a new .txt file
+                print(f"Found {len(cell_names)} cell name(s):")
+                for index, name in enumerate(cell_names):
+                    print(f"{index+1}. {name}")
+                
+                output_file_path = 'cell_names.txt'  # You can change this path as needed
+
+                with open(output_file_path, 'w') as output_file:
+                    for name in cell_names:
+                        output_file.write(name + "\n")
+
+    except FileNotFoundError:
+            print("Error: File not found")
+    except Exception as e:
+         print(f"An error occured: {str(e)}")
+
+extract_cell_names(input_file)
+
+# with open(input_file, 'r') as input:
+#     content = input.read()
+#     content.find("cell")
+
+
+
+# with open(output_file, 'w') as output:
+#     output.write("hello")
+
+
+
+
+
+
+
+
