91f5d11bb101093cda99596561e67f568973f0cf
Colin <cmchoi@usc.edu>
2024-09-07 15:05:57 -0700

    starting work

--- Test.py
+++ Test.py
@@ -6,32 +6,32 @@ input_file = 'LiberateResults.txt'
 # output_file = 'Results.txt'
 
 
-def extract_cell_names(input_file_path):
-    try:
-        with open(input_file_path, 'r') as input_file:
-                content = input_file.read()
+# def extract_cell_names(input_file_path):
+#     try:
+#         with open(input_file_path, 'r') as input_file:
+#                 content = input_file.read()
 
-                # Use a regular expression pattern to find "cell (**name**)" occurrences
-                pattern = r"cell \("
-                cell_names = re.findall(pattern, content)
+#                 # Use a regular expression pattern to find "cell (**name**)" occurrences
+#                 pattern = r"cell \("
+#                 cell_names = re.findall(pattern, content)
 
-                # Print the extracted cell names and write them to a new .txt file
-                print(f"Found {len(cell_names)} cell name(s):")
-                for index, name in enumerate(cell_names):
-                    print(f"{index+1}. {name}")
+#                 # Print the extracted cell names and write them to a new .txt file
+#                 print(f"Found {len(cell_names)} cell name(s):")
+#                 for index, name in enumerate(cell_names):
+#                     print(f"{index+1}. {name}")
                 
-                output_file_path = 'cell_names.txt'  # You can change this path as needed
+#                 output_file_path = 'cell_names.txt'  # You can change this path as needed
 
-                with open(output_file_path, 'w') as output_file:
-                    for name in cell_names:
-                        output_file.write(name + "\n")
+#                 with open(output_file_path, 'w') as output_file:
+#                     for name in cell_names:
+#                         output_file.write(name + "\n")
 
-    except FileNotFoundError:
-            print("Error: File not found")
-    except Exception as e:
-         print(f"An error occured: {str(e)}")
+#     except FileNotFoundError:
+#             print("Error: File not found")
+#     except Exception as e:
+#          print(f"An error occured: {str(e)}")
 
-extract_cell_names(input_file)
+# extract_cell_names(input_file)
 
 # with open(input_file, 'r') as input:
 #     content = input.read()
@@ -41,6 +41,8 @@ extract_cell_names(input_file)
 
 # with open(output_file, 'w') as output:
 #     output.write("hello")
+
+
 
 
 
