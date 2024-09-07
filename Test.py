7c65be7516665bb1833dff49ae1519159d6f316e
Colin <cmchoi@usc.edu>
2024-09-07 16:22:01 -0700

    isolated name and area

--- Test.py
+++ Test.py
@@ -47,34 +47,46 @@
 
 input_file = 'LiberateResults.txt'
 
-cell = False
+cell, area = False, False
+
+
+cellNames = []
+
+class logicCell:
+    def __init__(self,name,area):
+        self.name = name
+        self.area = area
+        self.pins = "pins"
+    
+
+
+
+# above = []
+# one = logicCell()
+# two = logicCell()
+
+# above.append(one)
+# above.append(two)
+# print(above[1].name)
+# above[-1].name = "howdy"
+# print(above[1].name)
+
+
 
 with open(input_file, 'r') as input:
     content = input.read().split('\n')          # content is a list but content[0] is a str
 
-    # words = content[93].split()
-
-    for x in content:
-        for words in x.split():
-            if cell == True:
-                print(words)
-                cell = False
+    for i,val in enumerate(content):
+        # print(i,val)
+        for j, words in enumerate(val.split()):
+            # print(j, words)
             if words == "cell":
-                cell = True
 
+                name = val.split()[j+1].replace("(","").replace(")","").replace("{","").replace("}","")
+                area = content[i+1].split()[1].replace(";","")
+                cellNames.append(logicCell(name, area))
                 
-        
-
-    
-
-    # for x in content.index:
-    #     if (content[x] == "c"):
-    #         print(content[x+1:x+4])
-
-
-
-
-
-
+for x in cellNames:
+    print(x.name, x.area)
 
 
