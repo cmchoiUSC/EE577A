37dfde80a35768b01c78739163d9bbd2822a620b
Colin <cmchoi@usc.edu>
2024-09-08 11:25:22 -0700

    Added copy of main script and have pin labeling done for part 2

--- Lab1/Test.py
+++ Lab1/Test.py
@@ -12,7 +12,12 @@ class logicCell:
     def __init__(self,name):
         self.name = name
         self.area = 0.00
-        self.pin = {}
+        self.pin = []
+
+class pin:
+        def __init__(self,name,function):
+            self.name = name
+            self.function = function
 
 
 
@@ -35,7 +40,7 @@ with open(input_file, 'r') as input:
                 name = val.split()[j+1].replace("(","").replace(")","").replace("{","").replace("}","")
         if "function" in val:
             function = val.replace("function : ", "").replace(";", "").replace("      ", "").replace("\"", "")
-            cellNames[-1].pin.update({name:function})
+            cellNames[-1].pin.append(pin(name,function))
 
 
 
@@ -43,25 +48,35 @@ with open(input_file, 'r') as input:
 #### Writing Code ####
 
 with open('Results.txt', 'w') as results:
-    print("Pin names per function: \n", file=results)
+    print("Pin names per function: \n", file=results)                               # Prints initial document line
+
+    for i in range(len(cellNames)):                                                 # Begin print section for each cell
+        print("Section " + cellNames[i].name + " pins:", file=results)              # Print name for each cell (first line)
+        
+        for j in range(len(cellNames[i].pin)):                                      # Looping through each cell
+            temp = cellNames[i].pin[j].function.replace("("," ").replace(")"," ").replace("{"," ").replace("}"," ").replace("!"," ").replace("+"," ").replace("*"," ").replace("^"," ").split()
+            pins = []                                                               # Creates an array to hold all of the pins
+            for p in temp:                                                          # For loop to add the cleaned up pins labels into the pins array
+                if pins.count(p) < 1: pins.append(p)                                # Only add the pin if it hasn't already been added before
+
+            print("  Function "  + cellNames[i].pin[j].name + " = " + cellNames[i].pin[j].function + "  ==> uses pins: ", end='', file=results)     # First half of pin print statement
+            for p in range(len(pins)):                                              # Printing through the pins array... need to make sure the last pin doesn't print with a comma
+                if p == len(pins)-1: print(pins[p], file=results)
+                else: print(pins[p] + ", ", end='', file=results)
+
+
+
+            #### Code for part II #####
+            gFunctions = cellNames
+            # print (pins)
+            for p in range(len(pins)):                                              # looping through the pins to replace letters is P0, P1, P2,...
+                gFunctions[i].pin[j].function = gFunctions[i].pin[j].function.replace(pins[p], "P" + str(p))
+                # print(gFunctions[i].pin[j].function.replace(pins[p], "P" + str(p)))
+            #### Code for part II #####
                 
-    for cells in cellNames:
-        print("Section " + cells.name + " pins:", file=results)
-        for x in range(len(cells.pin)):
-            temp = list(cells.pin.values())[x].replace("("," ").replace(")"," ").replace("{"," ").replace("}"," ").replace("!"," ").replace("+"," ").replace("*"," ").replace("^"," ").split()
-            pins = []
-            for p in temp:
-                if pins.count(p) < 1:
-                    pins.append(p)
 
-            print("  Function " + list(cells.pin.keys())[x] + " = " + list(cells.pin.values())[x] + "  ==> uses pins: ", end='', file=results)
-            for p in range(len(pins)):
-                if (p == len(pins)-1):
-                    print(pins[p], file=results)
-                else:
-                    print(pins[p] + ", ", end='', file=results)
-        print("Total area = " + cells.area + "\n", file=results)
 
+        print("Total area = " + cellNames[i].area + "\n", file=results)             # Print area for each cell (last line)
 
 
 
@@ -74,13 +89,18 @@ with open('Results.txt', 'w') as results:
     #### Bracket Code ####
 
     table = PrettyTable()
-    table.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-    table.add_row(["Adelaide", 1295, 1158259, 600.5])
-    table.add_row(["Brisbane", 5905, 1857594, 1146.4])
-    table.add_row(["Darwin", 112, 120900, 1714.7])
-    table.add_row(["Hobart", 1357, 205556, 619.5])
-    table.add_row(["Sydney", 2058, 4336374, 1214.8])
-    table.add_row(["Melbourne", 1566, 3806092, 646.9])
-    table.add_row(["Perth", 5386, 1554769, 869.4])
+    table.field_names = ["Expression", "Gates"]
+
+
+    for i in range(len(gFunctions)):  
+        for j in range(len(gFunctions[i].pin)):
+            table.add_row([gFunctions[i].pin[j].function, "['" + gFunctions[i].name + "']"])
+
+
+
     print("", file=results)
     print(table, file=results)
+
+
+
+
